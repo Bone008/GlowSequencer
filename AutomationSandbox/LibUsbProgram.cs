ccf98bdc6382cd409b40d2d6832a8079f2ab9d2c
LAPTOP-FELIX\Felix <->
2024-03-28 22:21:03 +0100

    removed operation result in favor for exceptions

--- AutomationSandbox/LibUsbProgram.cs
+++ AutomationSandbox/LibUsbProgram.cs
@@ -13,20 +13,19 @@ namespace AutomationSandbox
         static void Main(string[] args)
         {
             IClubConnection clubConnection = new ClubConnectionUtility();
-            if (!clubConnection.ListConnectedClubs().IsSuccessWithResult(out List<ConnectedDevice>? clubs))
-            {
-                return;
-            }
+            List<ConnectedDevice> clubs = clubConnection.ListConnectedClubs();
             foreach (var club in clubs!)
             {
-                Console.WriteLine($"ConnectedPortId: {club.connectedPortId}, name: {club.name}, group_name: {club.groupName}, program_name: {club.programName}");
+                Console.WriteLine(
+                    $"ConnectedPortId: {club.connectedPortId}, name: {club.name}, group_name: {club.groupName}, program_name: {club.programName}");
             }
+
             if (clubs.Count == 0)
             {
                 Console.WriteLine("No connected clubs found!");
                 return;
             }
-            
+
             //TestWriteAndReadName(clubConnection, clubs, "Testing new Name 4");
             //TestWriteAndReadGroupName(clubConnection, clubs, "AA");
             //TestWriteAndReadProgramName(clubConnection, clubs, "TestProgram Name 2");
@@ -37,51 +36,51 @@ namespace AutomationSandbox
             //TestWriteAndReadProgram(clubConnection, clubs, programBytes);
             //clubConnection.Start(clubs[0].connectedPortId);
             //TestAutoReadProgram(clubConnection, clubs);
-            
+
             //byte[] programBytes = TestGeneratingLongProgram();
             //foreach (ConnectedDevice club in clubs)
             //{
             //   Task.Run(() => TestTransmit(club, clubConnection, programBytes));
             //}
 
-            
+
             Console.WriteLine("Done!");
             Console.ReadKey(true);
         }
-        
+
         private static void TestTransmit(ConnectedDevice club, IClubConnection clubConnection, byte[] programBytes)
         {
-            Console.WriteLine(clubConnection.WriteProgram(club.connectedPortId, programBytes).ToString());
-            Console.WriteLine(clubConnection.WriteName(club.connectedPortId, "LongProgram").ToString());
+            clubConnection.WriteProgram(club.connectedPortId, programBytes);
+            clubConnection.WriteName(club.connectedPortId, "LongProgram");
             clubConnection.Start(club.connectedPortId);
         }
-        
+
         private static byte[] TestGeneratingProgram()
         {
             GloLoopCommand loopCommand = new GloLoopCommand(2);
             loopCommand.Commands.AddRange(new GloCommand[]
             {
-                new GloColorCommand(GloColor.FromRGB(20,0,0)),
+                new GloColorCommand(GloColor.FromRGB(20, 0, 0)),
                 new GloDelayCommand(100),
-                new GloColorCommand(GloColor.FromRGB(0,20,0)),
+                new GloColorCommand(GloColor.FromRGB(0, 20, 0)),
                 new GloDelayCommand(100),
-                new GloColorCommand(GloColor.FromRGB(0,0,20)),
+                new GloColorCommand(GloColor.FromRGB(0, 0, 20)),
                 new GloDelayCommand(100),
             });
-            
+
             GloCommandContainer program = new GloCommandContainer("Program", "END");
             program.Commands.AddRange(new GloCommand[]
             {
-                new GloColorCommand(GloColor.FromRGB(20,0,0)),
+                new GloColorCommand(GloColor.FromRGB(20, 0, 0)),
                 new GloDelayCommand(100),
-                new GloColorCommand(GloColor.FromRGB(0,20,0)),
+                new GloColorCommand(GloColor.FromRGB(0, 20, 0)),
                 new GloDelayCommand(1000),
-                new GloRampCommand(GloColor.FromRGB(0,0,20),100),
+                new GloRampCommand(GloColor.FromRGB(0, 0, 20), 100),
                 new GloRampCommand(GloColor.Black, 1000),
                 loopCommand,
-                new GloRampCommand(GloColor.FromRGB(20,20,0),1000),
-                new GloRampCommand(GloColor.FromRGB(0,20,20),1000),
-                new GloRampCommand(GloColor.FromRGB(20,0,20),1000),
+                new GloRampCommand(GloColor.FromRGB(20, 20, 0), 1000),
+                new GloRampCommand(GloColor.FromRGB(0, 20, 20), 1000),
+                new GloRampCommand(GloColor.FromRGB(20, 0, 20), 1000),
                 new GloDelayCommand(2000),
             });
             byte[] programBytes = ProgramConverter.ConvertToBytes(program);
@@ -89,75 +88,38 @@ namespace AutomationSandbox
             return programBytes;
         }
 
-        
-        private static void TestWriteAndReadProgram(IClubConnection clubConnection, List<ConnectedDevice> clubs, byte[] programBytes)
-        {
-            OperationResult writeProgramOr = clubConnection.WriteProgram(clubs[0].connectedPortId, programBytes);
-            if (writeProgramOr.IsSuccess)
-            {
-                Console.WriteLine("Written Program!");
-            }
-            else
-            {
-                Console.WriteLine("Failed to write program: " + writeProgramOr.ErrorMessage);
-            }
 
-            OperationResult<byte[]> readProgramOr = clubConnection.ReadProgram(clubs[0].connectedPortId, programBytes.Length);
-            if (readProgramOr.IsSuccess)
-            {
-                Console.WriteLine($"Read Program: {BitConverter.ToString(readProgramOr.Data)}");
-            }
-            else
-            {
-                Console.WriteLine("Failed to read program: " + readProgramOr.ErrorMessage);
-            }
-            byte[] readProgram = readProgramOr.Data!;
+        private static void TestWriteAndReadProgram(IClubConnection clubConnection, List<ConnectedDevice> clubs,
+            byte[] programBytes)
+        {
+            clubConnection.WriteProgram(clubs[0].connectedPortId, programBytes);
+            Console.WriteLine("Written Program!");
+
+            byte[] readProgram = clubConnection.ReadProgram(clubs[0].connectedPortId, programBytes.Length);
+            Console.WriteLine($"Read Program: {BitConverter.ToString(readProgram)}");
+
             if (!programBytes.SequenceEqual(readProgram))
             {
-                Console.WriteLine($"Program does not match the read program - Program: <{BitConverter.ToString(programBytes)}> != <{BitConverter.ToString(readProgram)}>");
+                Console.WriteLine("Program does not match the read program!");
             }
             else
             {
-                Console.WriteLine("Program matches the read program!");
-            }
-        }
-        
-        private static void TestWriteAndReadName(IClubConnection clubConnection, List<ConnectedDevice> clubs, string name = "TestName Longer")
-        {
-            OperationResult writeNameOr = clubConnection.WriteName(clubs[0].connectedPortId, name);
-            if (writeNameOr.IsSuccess)
-            {
-                Console.WriteLine($"Written Name: {name}");
-            }
-            else
-            {
-                Console.WriteLine("Failed to write name: " + name + "\n" + writeNameOr.ErrorMessage);
+                Console.WriteLine("Program matches the read program!");                
             }
 
-            OperationResult<string> readNameOr = clubConnection.ReadName(clubs[0].connectedPortId);
-            if (readNameOr.IsSuccess)
+        }
+
+        private static void TestWriteAndReadName(IClubConnection clubConnection, List<ConnectedDevice> clubs,
+            string name = "TestName Longer")
+        {
+            clubConnection.WriteName(clubs[0].connectedPortId, name);
+            Console.WriteLine($"Written Name: {name}");
+
+            string readName = clubConnection.ReadName(clubs[0].connectedPortId);
+            Console.WriteLine($"Read Name: {readName}");
+
+            if (!string.Equals(name, readName))
             {
-                Console.WriteLine($"Read Name: {readNameOr.Data}");
-            }
-            else
-            {
-                Console.WriteLine("Failed to read name: " + "\n" + readNameOr.ErrorMessage);
-            }
-            string readName = readNameOr.Data!;
-            if (!string.Equals(name,readName))
-            {
-                Console.WriteLine($"Debug - Name: |{name}|, ReadName: |{readName}|");
-                foreach (char c in name)
-                {
-                    Console.Write($"{(int)c} ");
-                }
-                Console.WriteLine();
-                foreach (char c in readName)
-                {
-                    Console.Write($"{(int)c} ");
-                }
-                Console.WriteLine();
-                
                 Console.WriteLine($"Name does not match the read name - Name: <{name}> != <{readName}>");
             }
             else
@@ -165,153 +127,71 @@ namespace AutomationSandbox
                 Console.WriteLine("Name matches the read name!");
             }
         }
-        
-        private static void TestWriteAndReadGroupName(IClubConnection clubConnection, List<ConnectedDevice> clubs, string groupName = "Test")
-        {
-            OperationResult writeGroupNameOr = clubConnection.WriteGroupName(clubs[0].connectedPortId, groupName);
-            if (writeGroupNameOr.IsSuccess)
-            {
-                Console.WriteLine($"Written GroupName: {groupName}");
-            }
-            else
-            {
-                Console.WriteLine("Failed to write group name: " + groupName + "\n" + writeGroupNameOr.ErrorMessage);
-            }
 
-            OperationResult<string> readGroupNameOr = clubConnection.ReadGroupName(clubs[0].connectedPortId);
-            if (readGroupNameOr.IsSuccess)
+        private static void TestWriteAndReadGroupName(IClubConnection clubConnection, List<ConnectedDevice> clubs,
+            string groupName = "Test")
+        {
+            clubConnection.WriteGroupName(clubs[0].connectedPortId, groupName);
+            Console.WriteLine($"Written GroupName: {groupName}");
+
+            string readGroupName = clubConnection.ReadGroupName(clubs[0].connectedPortId);
+            Console.WriteLine($"Read GroupName: {readGroupName}");
+
+            if (!string.Equals(groupName, readGroupName))
             {
-                Console.WriteLine($"Read GroupName: {readGroupNameOr.Data}");
-            }
-            else
-            {
-                Console.WriteLine("Failed to read group name: " + "\n" + readGroupNameOr.ErrorMessage);
-            }
-            string readGroupName = readGroupNameOr.Data!;
-            if (!string.Equals(groupName,readGroupName))
-            {
-                Console.WriteLine($"Debug - GroupName: |{groupName}|, ReadGroupName: |{readGroupName}|");
-                foreach (char c in groupName)
-                {
-                    Console.Write($"{(int)c} ");
-                }
-                Console.WriteLine();
-                foreach (char c in readGroupName)
-                {
-                    Console.Write($"{(int)c} ");
-                }
-                Console.WriteLine();
-                
-                Console.WriteLine($"GroupName does not match the read group name - GroupName: <{groupName}> != <{readGroupName}>");
+                Console.WriteLine(
+                    $"GroupName does not match the read group name - GroupName: <{groupName}> != <{readGroupName}>");
             }
             else
             {
                 Console.WriteLine("GroupName matches the read group name!");
             }
         }
-        
-        private static void TestWriteAndReadProgramName(IClubConnection clubConnection, List<ConnectedDevice> clubs, string programName = "TestProgram")
-        {
-            OperationResult writeProgramNameOr = clubConnection.WriteProgramName(clubs[0].connectedPortId, programName);
-            if (writeProgramNameOr.IsSuccess)
-            {
-                Console.WriteLine($"Written ProgramName: {programName}");
-            }
-            else
-            {
-                Console.WriteLine("Failed to write program name: " + programName + "\n" + writeProgramNameOr.ErrorMessage);
-            }
 
-            OperationResult<string> readProgramNameOr = clubConnection.ReadProgramName(clubs[0].connectedPortId);
-            if (readProgramNameOr.IsSuccess)
+        private static void TestWriteAndReadProgramName(IClubConnection clubConnection, List<ConnectedDevice> clubs,
+            string programName = "TestProgram")
+        {
+            clubConnection.WriteProgramName(clubs[0].connectedPortId, programName);
+            Console.WriteLine($"Written ProgramName: {programName}");
+
+            string readProgramName = clubConnection.ReadProgramName(clubs[0].connectedPortId);
+            Console.WriteLine($"Read ProgramName: {readProgramName}");
+
+            if (!string.Equals(programName, readProgramName))
             {
-                Console.WriteLine($"Read ProgramName: {readProgramNameOr.Data}");
-            }
-            else
-            {
-                Console.WriteLine("Failed to read program name: " + "\n" + readProgramNameOr.ErrorMessage);
-            }
-            string readProgramName = readProgramNameOr.Data!;
-            if (!string.Equals(programName,readProgramName))
-            {
-                Console.WriteLine($"Debug - ProgramName: |{programName}|, ReadProgramName: |{readProgramName}|");
-                foreach (char c in programName)
-                {
-                    Console.Write($"{(int)c} ");
-                }
-                Console.WriteLine();
-                foreach (char c in readProgramName)
-                {
-                    Console.Write($"{(int)c} ");
-                }
-                Console.WriteLine();
-                
-                Console.WriteLine($"ProgramName does not match the read program name - ProgramName: <{programName}> != <{readProgramName}>");
+                Console.WriteLine(
+                    $"ProgramName does not match the read program name - ProgramName: <{programName}> != <{readProgramName}>");
             }
             else
             {
                 Console.WriteLine("ProgramName matches the read program name!");
             }
         }
-        
+
         private static void TestAutoReadProgram(IClubConnection clubConnection, List<ConnectedDevice> clubs)
         {
-            OperationResult<byte[]> readProgramOr = clubConnection.ReadProgramAutoDetect(clubs[0].connectedPortId);
-            if (readProgramOr.IsSuccess)
-            {
-                Console.WriteLine($"Read Program: {BitConverter.ToString(readProgramOr.Data)}");
-            }
-            else
-            {
-                Console.WriteLine("Failed to read program: " + readProgramOr.ErrorMessage);
-            }
+            byte[] readProgram = clubConnection.ReadProgramAutoDetect(clubs[0].connectedPortId);
+            Console.WriteLine($"Read Program: {BitConverter.ToString(readProgram)}");
         }
-        
+
         private static void TestStartAndStop(IClubConnection clubConnection, List<ConnectedDevice> clubs)
         {
-            OperationResult startOr = clubConnection.Start(clubs[0].connectedPortId);
-            if (startOr.IsSuccess)
-            {
-                Console.WriteLine("Started!");
-            }
-            else
-            {
-                Console.WriteLine("Failed to start: " + startOr.ErrorMessage);
-            }
+            clubConnection.Start(clubs[0].connectedPortId);
+            Console.WriteLine("Started!");
             Thread.Sleep(2000);
-            OperationResult stopOr = clubConnection.Stop(clubs[0].connectedPortId);
-            if (stopOr.IsSuccess)
-            {
-                Console.WriteLine("Stopped!");
-            }
-            else
-            {
-                Console.WriteLine("Failed to stop: " + stopOr.ErrorMessage);
-            }
+            clubConnection.Stop(clubs[0].connectedPortId);
+            Console.WriteLine("Stopped!");
         }
-        
+
         private static void TestSetColorAndStop(IClubConnection clubConnection, List<ConnectedDevice> clubs)
         {
-            OperationResult setColorOr = clubConnection.SetColor(clubs[0].connectedPortId, 255, 0, 0);
-            if (setColorOr.IsSuccess)
-            {
-                Console.WriteLine("Color set!");
-            }
-            else
-            {
-                Console.WriteLine("Failed to set color: " + setColorOr.ErrorMessage);
-            }
+            clubConnection.SetColor(clubs[0].connectedPortId, 255, 0, 0);
+            Console.WriteLine("Color set!");
             Thread.Sleep(2000);
-            OperationResult stopOr = clubConnection.Stop(clubs[0].connectedPortId);
-            if (stopOr.IsSuccess)
-            {
-                Console.WriteLine("Stopped!");
-            }
-            else
-            {
-                Console.WriteLine("Failed to stop: " + stopOr.ErrorMessage);
-            }
+            clubConnection.Stop(clubs[0].connectedPortId);
+            Console.WriteLine("Stopped!");
         }
+
 
         private static byte[] TestGeneratingLongProgram()
         {
--- GlowSequencer/Usb/CommunicationUtility.cs
+++ GlowSequencer/Usb/CommunicationUtility.cs
@@ -15,8 +15,8 @@ namespace GlowSequencer.Usb
         {
             public byte command;
             public byte dataLength;
-            public byte addressLe0; // LSB
-            public byte addressLe1; // MSB
+            private byte addressLe0; // LSB
+            private byte addressLe1; // MSB
             public byte unknown1;//0x00
             public byte unknown2;//0x00
 
@@ -46,39 +46,37 @@ namespace GlowSequencer.Usb
                 }
             }
         }
-        public static OperationResult<TransferHeader> CreateHeader(byte[] headerData)
+        public static TransferHeader CreateHeader(byte[] headerData)
         {
             if (headerData.Length < 4)
             {
-                return OperationResult<TransferHeader>.Fail("Header data is too short!");
+                throw new Exception("Header data is too short!");
             }
 
             TransferHeader header = new TransferHeader()
             {
                 command = headerData[0],
                 dataLength = headerData[1],
-                addressLe0 = headerData[2],
-                addressLe1 = headerData[3],
                 unknown1 = 0x00,
                 unknown2 = 0x00,
             };
+            header.Address = (ushort)((headerData[3] << 8) | headerData[2]);
 
             if (headerData.Length == 4)
             {
-                return OperationResult<TransferHeader>.Success(header);
+                return header;
             }
 
             if (headerData.Length == 6)
             {
                 header.unknown1 = headerData[4];
                 header.unknown2 = headerData[5];
-                return OperationResult<TransferHeader>.Success(header);
+                return header;
             }
-
-            return OperationResult<TransferHeader>.Fail("Header data length does not match!");
+            throw new Exception("Header data length does not match!");
         }
 
-        public static OperationResult<byte[]> ReadContinuously(UsbDevice device, TransferHeader header, int amount)
+        public static byte[] ReadContinuously(UsbDevice device, TransferHeader header, int amount)
         {
             int startAddress = header.Address;
             byte[] result = new byte[header.dataLength * amount];
@@ -86,15 +84,12 @@ namespace GlowSequencer.Usb
             {
                 header.Address = (ushort)(startAddress + (i * header.dataLength));
                 byte[] headerBuffer = header.AsBuffer;
-                if (WriteReadBulk(device, headerBuffer, header.dataLength + 6).IsFail(out OperationResult<byte[]?> readResult))
-                {
-                    return OperationResult<byte[]>.Fail($"Continuous Read failed in block {i}", readResult.ErrorMessage)!;
-                }
-                byte[] readBuffer = readResult.Data!;
+                byte[] readBuffer = WriteReadBulk(device, headerBuffer, header.dataLength + 6);
+
                 Array.Copy(readBuffer, 6, result, (i * header.dataLength), header.dataLength);
             }
 
-            return OperationResult<byte[]>.Success(result);
+            return result;
         }
 
         public static IEnumerable<byte[]> ReadContinuouslyEnumerable(UsbDevice device, TransferHeader header)
@@ -105,14 +100,16 @@ namespace GlowSequencer.Usb
             {
                 header.Address = (ushort)(startAddress + (i * header.dataLength));
                 byte[] headerBuffer = header.AsBuffer;
-
-                var writeReadResult = WriteReadBulk(device, headerBuffer, header.dataLength + 6);
-                if (writeReadResult.IsFail(out OperationResult<byte[]?> readResult))
+                byte[] readBuffer = null;
+                try
                 {
-                    throw new Exception($"Continuous Read failed in block {i}. Error: {readResult.ErrorMessage}");
+                    readBuffer = WriteReadBulk(device, headerBuffer, header.dataLength + 6);
                 }
-
-                byte[] readBuffer = readResult.Data!;
+                catch (Exception e)
+                {
+                    throw new Exception($"Continuous Read failed in block {i} - {e.Message}");
+                }
+;
                 byte[] chunk = new byte[header.dataLength];
                 Array.Copy(readBuffer, 6, chunk, 0, header.dataLength);
 
@@ -122,7 +119,7 @@ namespace GlowSequencer.Usb
         }
 
 
-        public static OperationResult WriteContinuously(UsbDevice device, TransferHeader header, byte[] data, int amount, byte[] expectedReturn)
+        public static void WriteContinuously(UsbDevice device, TransferHeader header, byte[] data, int amount, byte[] expectedReturn)
         {
             //pad data to match header.dataLength * amount
             if (data.Length < header.dataLength * amount)
@@ -141,37 +138,23 @@ namespace GlowSequencer.Usb
                 Array.Copy(headerBuffer, writeBuffer, headerBuffer.Length);
                 Array.Copy(data, (i * header.dataLength), writeBuffer, headerBuffer.Length, header.dataLength);
                 Console.WriteLine($"Writing: {BitConverter.ToString(writeBuffer)}");
-                if (WriteReadBulk(device, writeBuffer, expectedReturn.Length).IsFail(out OperationResult<byte[]?> writeResult))
-                {
-                    return OperationResult.Fail($"Continuous Write failed in block {i}", writeResult.ErrorMessage);
-                }
-                byte[] readBuffer = writeResult.Data!;
+                byte[] readBuffer = WriteReadBulk(device, writeBuffer, expectedReturn.Length);
+                
                 if (expectedReturn.Length > 0)
                 {
                     for (int j = 0; j < expectedReturn.Length; j++)
                     {
                         if (readBuffer[j] != expectedReturn[j])
                         {
-                            return OperationResult.Fail($"Expected return value not found! Expected: {BitConverter.ToString(expectedReturn)}, found: {BitConverter.ToString(readBuffer)}");
+                            throw new Exception($"Expected return value not found! Expected: {BitConverter.ToString(expectedReturn)}, found: {BitConverter.ToString(readBuffer)}");
                         }
                     }
                 }
             }
-
-            return OperationResult.Success();
         }
 
-        public static OperationResult<byte[]?> WriteReadBulk(UsbDevice device, byte[] writeBuffer, int readBufferSize)
+        public static byte[] WriteReadBulk(UsbDevice device, byte[] writeBuffer, int readBufferSize)
         {
-            if (WriteBulk(device, writeBuffer).IsFail(out OperationResult writeResult))
-            {
-                return OperationResult<byte[]>.Fail(writeResult.ErrorMessage);
-            }
-
-            if (ReadBulk(device, readBufferSize).IsFail(out OperationResult<byte[]?> readResult))
-            {
-                return OperationResult<byte[]>.Fail(readResult.ErrorMessage);
-            }
 #if SIMULATE_RW
             byte[] simulatedResultBuffer = new byte[readBufferSize];
             //copy writeBuffer to simulate read - pad or truncate if necessary
@@ -179,15 +162,17 @@ namespace GlowSequencer.Usb
             Console.WriteLine($"write_read bulk sim result: {BitConverter.ToString(simulatedResultBuffer)}");
             return OperationResult<byte[]>.Success(simulatedResultBuffer)!;
 #else
-            return OperationResult<byte[]>.Success(readResult.Data!)!;
+            WriteBulk(device, writeBuffer);
+            var result = ReadBulk(device, readBufferSize);
+            return result;
 #endif
         }
 
-        private static OperationResult WriteBulk(UsbDevice device, byte[] buffer)
+        private static void WriteBulk(UsbDevice device, byte[] buffer)
         {
             if (buffer.Length == 0)
             {
-                return OperationResult.Fail("Buffer is empty!");
+                throw new Exception("Buffer is empty!");
             }
 #if SIMULATE_RW
             Console.WriteLine($"Simulating write bulk {BitConverter.ToString(buffer)}");
@@ -197,13 +182,13 @@ namespace GlowSequencer.Usb
                 .Write(buffer, 1000, out int transferLength);
             if (errorCode == ErrorCode.Success)
             {
-                return OperationResult.Success();
+                return;
             }
-            return OperationResult.Fail($"write bulk {BitConverter.ToString(buffer)} resulted in {errorCode.ToString()}");
+            throw new Exception($"write bulk {BitConverter.ToString(buffer)} resulted in {errorCode.ToString()}");
 #endif
         }
 
-        private static OperationResult<byte[]?> ReadBulk(UsbDevice device, int readBufferSize)
+        private static byte[] ReadBulk(UsbDevice device, int readBufferSize)
         {
             byte[] buffer = new byte[readBufferSize];
 #if SIMULATE_RW
@@ -214,13 +199,13 @@ namespace GlowSequencer.Usb
                 .Read(buffer, 1000, out int transferLength);
             if (errorCode == ErrorCode.Success)
             {
-                return OperationResult<byte[]>.Success(buffer)!;
+                return buffer;
             }
-            return OperationResult<byte[]>.Fail($"read bulk: {BitConverter.ToString(buffer)} with code {errorCode.ToString()}");
+            throw new Exception($"read bulk: {BitConverter.ToString(buffer)} with code {errorCode.ToString()}");
 #endif
         }
 
-        public static OperationResult WriteControl(UsbDevice device)
+        public static void WriteControl(UsbDevice device)
         {
 #if SIMULATE_RW
             Console.WriteLine("Simulating control transfer");
@@ -230,9 +215,9 @@ namespace GlowSequencer.Usb
             bool success = device.ControlTransfer(ref packet, null, 0, out _);
             if (success)
             {
-                return OperationResult.Success();
+                return;
             }
-            return OperationResult.Fail($"Control transfer failed for {packet.ToString()}!");
+            throw new Exception($"Control transfer failed for {packet.ToString()}!");
 #endif
         }
     }
